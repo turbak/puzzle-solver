3ea235f9897b99f61fed35319b1b0418f72947b1
Ivan Shcherbina <55194117+turbak@users.noreply.github.com>
2025-03-02 16:41:28 +0000

    feat: added concurrency

--- other_languages/go/algo.go
+++ other_languages/go/algo.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type PieceAndPosition struct {
 	Piece
 	position
@@ -26,21 +24,29 @@ func solve(month string, day string) ([]PieceAndPosition, error) {
 		}
 	}
 
-	g := newGrid([]position{monthPos, dayPos})
-	res, solved := solveHelper(g, 0)
-	if !solved {
-		return nil, fmt.Errorf("no solution found for %v", []position{monthPos, dayPos})
+	resCh := make(chan []PieceAndPosition)
+
+	for i := range allPiecesWithTranspositions[0] {
+		go func(pos int) {
+			g := newGrid([]position{monthPos, dayPos})
+			res, solved := solveHelper(g, 0, pos)
+			if solved {
+				select {
+				case resCh <- res:
+				}
+			}
+		}(i)
 	}
 
-	return res, nil
+	return <-resCh, nil
 }
 
-func solveHelper(g Grid, pieceIdx int) ([]PieceAndPosition, bool) {
+func solveHelper(g Grid, pieceIdx int, startPos int) ([]PieceAndPosition, bool) {
 	if pieceIdx >= len(allPiecesWithTranspositions) {
 		return make([]PieceAndPosition, 0, len(allPiecesWithTranspositions)), true
 	}
 
-	for _, pieceTransposition := range allPiecesWithTranspositions[pieceIdx] {
+	for _, pieceTransposition := range allPiecesWithTranspositions[pieceIdx][startPos:] {
 		for i := uint64(0); i < gridHeight-pieceTransposition.height+1; i++ {
 			for j := uint64(0); j < gridWidth-pieceTransposition.width+1; j++ {
 				if !g.CanPlace(pieceTransposition, int(i), int(j)) {
@@ -49,7 +55,7 @@ func solveHelper(g Grid, pieceIdx int) ([]PieceAndPosition, bool) {
 
 				gridCopy := g
 				gridCopy.Place(pieceTransposition, int(i), int(j))
-				if res, solved := solveHelper(gridCopy, pieceIdx+1); solved {
+				if res, solved := solveHelper(gridCopy, pieceIdx+1, 0); solved {
 					return append(res, PieceAndPosition{
 						Piece: pieceTransposition,
 						position: position{
--- other_languages/go/main.go
+++ other_languages/go/main.go
@@ -8,13 +8,6 @@ import (
 )
 
 func main() {
-	for _, pieces := range allPiecesWithTranspositions {
-		for _, piece := range pieces {
-			fmt.Println(piece)
-			fmt.Println()
-		}
-	}
-
 	solution, err := solve("jan", "1")
 	if err != nil {
 		log.Fatal(err)
